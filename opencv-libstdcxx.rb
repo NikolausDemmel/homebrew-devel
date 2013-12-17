7cfcf89175ed100720916294f08ca57bbe14d41f
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2013-12-17 15:46:26 +0100

    added opencv compiled with libstdc++

---
+++ opencv-libstdcxx.rb
@@ -0,0 +1,74 @@
+require 'formula'
+
+class OpencvLibstdcxx < Formula
+  homepage 'http://opencv.org/'
+  url 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz'
+  sha1 'e015bd67218844b38daf3cea8aab505b592a66c0'
+
+  keg_only "Only for development where libstdc++ is requeired."
+
+  option '32-bit'
+  option 'with-qt',  'Build the Qt4 backend to HighGUI'
+  option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
+  option 'without-opencl', 'Disable gpu code in OpenCV using OpenCL'
+
+  depends_on 'cmake' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'numpy' => :python
+  depends_on :python
+
+  depends_on 'eigen'   => :optional
+  depends_on 'libtiff' => :optional
+  depends_on 'jasper'  => :optional
+  depends_on 'tbb'     => :optional
+  depends_on 'qt'      => :optional
+  depends_on :libpng
+
+
+  # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
+  # you don't need unless you're doing video analysis, and some of it isn't
+  # in Homebrew anyway. Will depend on openexr if it's installed.
+  depends_on 'ffmpeg' => :optional
+
+  def install
+    ENV.append_to_cflags "-stdlib=libstdc++"
+
+    args = std_cmake_args + %W[
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=
+      -DWITH_CUDA=OFF
+      -DBUILD_ZLIB=OFF
+      -DBUILD_TIFF=OFF
+      -DBUILD_PNG=OFF
+      -DBUILD_JPEG=OFF
+      -DBUILD_JASPER=OFF
+      -DBUILD_TESTS=OFF
+      -DBUILD_PERF_TESTS=OFF
+      -DPYTHON_INCLUDE_DIR='#{python.incdir}'
+      -DPYTHON_LIBRARY='#{python.libdir}/lib#{python.xy}.dylib'
+      -DPYTHON_EXECUTABLE='#{python.binary}'
+    ]
+
+    if build.build_32_bit?
+      args << "-DCMAKE_OSX_ARCHITECTURES=i386"
+      args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
+      args << "-DOPENCV_EXTRA_CXX_FLAGS='-arch i386 -m32'"
+    end
+    args << '-DWITH_QT=ON' if build.with? 'qt'
+    args << '-DWITH_TBB=ON' if build.with? 'tbb'
+    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
+    args << '-DWITH_OPENCL=OFF' if build.without? 'opencl' or MacOS.version < :lion
+    args << '-DWITH_FFMPEG=OFF' unless build.with? 'ffmpeg'
+
+    args << '..'
+    mkdir 'macbuild' do
+      system 'cmake', *args
+      system "make"
+      system "make install"
+    end
+  end
+
+
+  def caveats
+    python.standard_caveats if python
+  end
+end
