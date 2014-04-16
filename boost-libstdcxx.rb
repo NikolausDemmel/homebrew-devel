ec6caaaf2f39868a8ba3f7fbd67229bfc842e3bc
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-04-16 12:39:09 +0200

    added libstdcxx versions of std libs for cuda + opencv development

---
+++ boost-libstdcxx.rb
@@ -0,0 +1,169 @@
+require 'formula'
+
+class UniversalPython < Requirement
+  satisfy(:build_env => false) { archs_for_command("python").universal? }
+
+  def message; <<-EOS.undent
+    A universal build was requested, but Python is not a universal build
+
+    Boost compiles against the Python it finds in the path; if this Python
+    is not a universal build then linking will likely fail.
+    EOS
+  end
+end
+
+class BoostLibstdcxx < Formula
+  homepage 'http://www.boost.org'
+  url 'https://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2'
+  sha1 'cef9a0cc7084b1d639e06cd3bc34e4251524c840'
+
+  keg_only "Only for development where libstdc++ is requeired."
+
+  head 'https://github.com/boostorg/boost.git'
+
+  env :userpaths
+
+  option :universal
+  option 'with-icu', 'Build regexp engine with icu support'
+  option 'without-single', 'Disable building single-threading variant'
+  option 'without-static', 'Disable building static library variant'
+  option 'with-mpi', 'Build with MPI support'
+
+  depends_on :python => :recommended
+  depends_on UniversalPython if build.universal? and build.with? "python"
+
+  if build.with? 'icu'
+    if build.cxx11?
+      depends_on 'icu4c' => 'c++11'
+    else
+      depends_on 'icu4c'
+    end
+  end
+
+  if build.with? 'mpi'
+    if build.cxx11?
+      depends_on 'open-mpi' => 'c++11'
+    else
+      depends_on :mpi => [:cc, :cxx, :optional]
+    end
+  end
+
+  stable do
+    # Patches boost::atomic for LLVM 3.4 as it is used on OS X 10.9 with Xcode 5.1
+    # https://github.com/Homebrew/homebrew/issues/27396
+    # https://github.com/Homebrew/homebrew/pull/27436
+    patch :p2 do
+      url "https://github.com/boostorg/atomic/commit/6bb71fdd.patch"
+      sha1 "8dfaf4d123f1161c83fff91d4ef70b8f95a4ef94"
+    end
+
+    patch :p2 do
+      url "https://github.com/boostorg/atomic/commit/e4bde20f.patch"
+      sha1 "f206e7261d00503788ae8ec3a0635ced8a816293"
+    end
+  end
+
+  fails_with :llvm do
+    build 2335
+    cause "Dropped arguments to functions when linking with boost"
+  end
+
+  def install
+    # https://svn.boost.org/trac/boost/ticket/8841
+    if build.with? 'mpi' and build.with? 'single'
+      raise <<-EOS.undent
+        Building MPI support for both single and multi-threaded flavors
+        is not supported.  Please use '--with-mpi' together with
+        '--without-single'.
+      EOS
+    end
+
+    ENV.universal_binary if build.universal?
+
+    # Force boost to compile using the appropriate GCC version.
+    open("user-config.jam", "a") do |file|
+      file.write "using darwin : : #{ENV.cxx} ;\n"
+      file.write "using mpi ;\n" if build.with? 'mpi'
+    end
+
+    # we specify libdir too because the script is apparently broken
+    bargs = ["--prefix=#{prefix}", "--libdir=#{lib}"]
+
+    if build.with? 'icu'
+      icu4c_prefix = Formula['icu4c'].opt_prefix
+      bargs << "--with-icu=#{icu4c_prefix}"
+    else
+      bargs << '--without-icu'
+    end
+
+    # Handle libraries that will not be built.
+    without_libraries = []
+
+    # The context library is implemented as x86_64 ASM, so it
+    # won't build on PPC or 32-bit builds
+    # see https://github.com/Homebrew/homebrew/issues/17646
+    if Hardware::CPU.ppc? || Hardware::CPU.is_32_bit? || build.universal?
+      without_libraries << "context"
+      # The coroutine library depends on the context library.
+      without_libraries << "coroutine"
+    end
+
+    # Boost.Log cannot be built using Apple GCC at the moment. Disabled
+    # on such systems.
+    without_libraries << "log" if ENV.compiler == :gcc || ENV.compiler == :llvm
+
+    without_libraries << "python" if build.without? 'python'
+    without_libraries << "mpi" if build.without? 'mpi'
+
+    bargs << "--without-libraries=#{without_libraries.join(',')}"
+
+    args = ["--prefix=#{prefix}",
+            "--libdir=#{lib}",
+            "-d2",
+            "-j#{ENV.make_jobs}",
+            "--layout=tagged",
+            "--user-config=user-config.jam",
+            "install"]
+
+    if build.with? "single"
+      args << "threading=multi,single"
+    else
+      args << "threading=multi"
+    end
+
+    if build.with? "static"
+      args << "link=shared,static"
+    else
+      args << "link=shared"
+    end
+
+    args << "address-model=32_64" << "architecture=x86" << "pch=off" if build.universal?
+
+    args << "cxxflags=-stdlib=libstdc++" << "linkflags=-stdlib=libstdc++"
+
+    system "./bootstrap.sh", *bargs
+    system "./b2", *args
+  end
+
+  def caveats
+    s = ''
+    # ENV.compiler doesn't exist in caveats. Check library availability
+    # instead.
+    if Dir.glob("#{lib}/libboost_log*").empty?
+      s += <<-EOS.undent
+
+      Building of Boost.Log is disabled because it requires newer GCC or Clang.
+      EOS
+    end
+
+    if Hardware::CPU.ppc? || Hardware::CPU.is_32_bit? || build.universal?
+      s += <<-EOS.undent
+
+      Building of Boost.Context and Boost.Coroutine is disabled as they are
+      only supported on x86_64.
+      EOS
+    end
+
+    s
+  end
+end
--- opencv-libstdcxx.rb
+++ opencv-libstdcxx.rb
@@ -2,28 +2,27 @@ require 'formula'
 
 class OpencvLibstdcxx < Formula
   homepage 'http://opencv.org/'
-  url 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz'
-  sha1 'e015bd67218844b38daf3cea8aab505b592a66c0'
+  url 'https://github.com/Itseez/opencv/archive/2.4.8.2.tar.gz'
+  sha1 '15c91f8fc806d39735ac4ce6a1c381541d4b5c80'
 
   keg_only "Only for development where libstdc++ is requeired."
 
-  option '32-bit'
-  option 'with-qt',  'Build the Qt4 backend to HighGUI'
-  option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
-  option 'without-opencl', 'Disable gpu code in OpenCV using OpenCL'
+  option "32-bit"
+  option "with-java", "Build with Java support"
+  option "with-qt", "Build the Qt4 backend to HighGUI"
+  option "with-tbb", "Enable parallel code in OpenCV using Intel TBB"
+  option "with-tests", "Build with accuracy & performance tests"
+  option "without-opencl", "Disable GPU code in OpenCV using OpenCL"
 
-  depends_on 'cmake' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'numpy' => :python
+  depends_on :ant if build.with? "java"
+  depends_on "cmake"      => :build
+  depends_on "eigen"      => :recommended
+  depends_on "numpy"      => :python
+  depends_on "openni"     => :optional
+  depends_on "pkg-config" => :build
   depends_on :python
-
-  depends_on 'eigen'   => :optional
-  depends_on 'libtiff' => :optional
-  depends_on 'jasper'  => :optional
-  depends_on 'tbb'     => :optional
-  depends_on 'qt'      => :optional
-  depends_on :libpng
-
+  depends_on "qt"         => :optional
+  depends_on "tbb"        => :optional
 
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
@@ -33,42 +32,60 @@ class OpencvLibstdcxx < Formula
   def install
     ENV.append_to_cflags "-stdlib=libstdc++"
 
-    args = std_cmake_args + %W[
+    py_prefix = %x(python-config --prefix).chomp
+
+    args = std_cmake_args + %W(
       -DCMAKE_OSX_DEPLOYMENT_TARGET=
       -DWITH_CUDA=OFF
-      -DBUILD_ZLIB=OFF
-      -DBUILD_TIFF=OFF
-      -DBUILD_PNG=OFF
-      -DBUILD_JPEG=OFF
-      -DBUILD_JASPER=OFF
-      -DBUILD_TESTS=OFF
-      -DBUILD_PERF_TESTS=OFF
-      -DPYTHON_INCLUDE_DIR='#{python.incdir}'
-      -DPYTHON_LIBRARY='#{python.libdir}/lib#{python.xy}.dylib'
-      -DPYTHON_EXECUTABLE='#{python.binary}'
-    ]
+      -DBUILD_ZLIB=ON
+      -DBUILD_TIFF=ON
+      -DBUILD_PNG=ON
+      -DBUILD_OPENEXR=ON
+      -DBUILD_JASPER=ON
+      -DBUILD_JPEG=ON
+      -DPYTHON_LIBRARY=#{py_prefix}/Python
+      -DPYTHON_INCLUDE_DIR=#{py_prefix}/Headers
+    )
 
-    if build.build_32_bit?
+    if build.without? "tests"
+      args << "-DBUILD_TESTS=OFF" << "-DBUILD_PERF_TESTS=OFF"
+    end
+
+    args << "-DBUILD_opencv_java=" + ((build.with? "java") ? "ON" : "OFF")
+    args << "-DWITH_OPENEXR=" + ((build.with? "openexr") ? "ON" : "OFF")
+    args << "-DWITH_QT=" + ((build.with? "qt") ? "ON" : "OFF")
+    args << "-DWITH_TBB=" + ((build.with? "tbb") ? "ON" : "OFF")
+    args << "-DWITH_FFMPEG=" + ((build.with? "ffmpeg") ? "ON" : "OFF")
+
+    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
+    args << "-DWITH_OPENCL=OFF" if build.without? "opencl" or MacOS.version < :lion
+
+    if build.with? "openni"
+      args << "-DWITH_OPENNI=ON"
+      # Set proper path for Homebrew's openni
+      inreplace "cmake/OpenCVFindOpenNI.cmake" do |s|
+        s.gsub! "/usr/include/ni", "#{Formula["openni"].opt_include}/ni"
+        s.gsub! "/usr/lib", "#{Formula["openni"].opt_lib}"
+      end
+    end
+
+    if build.include? "32-bit"
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
       args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
       args << "-DOPENCV_EXTRA_CXX_FLAGS='-arch i386 -m32'"
     end
-    args << '-DWITH_QT=ON' if build.with? 'qt'
-    args << '-DWITH_TBB=ON' if build.with? 'tbb'
-    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
-    args << '-DWITH_OPENCL=OFF' if build.without? 'opencl' or MacOS.version < :lion
-    args << '-DWITH_FFMPEG=OFF' unless build.with? 'ffmpeg'
 
-    args << '..'
-    mkdir 'macbuild' do
-      system 'cmake', *args
+    if ENV.compiler == :clang and !build.bottle?
+      args << '-DENABLE_SSSE3=ON' if Hardware::CPU.ssse3?
+      args << '-DENABLE_SSE41=ON' if Hardware::CPU.sse4?
+      args << '-DENABLE_SSE42=ON' if Hardware::CPU.sse4_2?
+      args << '-DENABLE_AVX=ON' if Hardware::CPU.avx?
+    end
+
+    mkdir "macbuild" do
+      system "cmake", "..", *args
       system "make"
       system "make install"
     end
-  end
-
-
-  def caveats
-    python.standard_caveats if python
   end
 end
