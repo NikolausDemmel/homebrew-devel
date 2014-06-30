8419804e9a22747c58e67d46314b8e8a981317f1
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-06-30 12:17:33 +0200

    qt-creator => 3.1.2

--- qt-creator.rb
+++ qt-creator.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class QtCreator < Formula
   homepage "https://qt-project.org/wiki/Category:Tools::QtCreator"
-  url "http://download.qt-project.org/official_releases/qtcreator/3.1/3.1.0/qt-creator-opensource-src-3.1.0.tar.gz"
-  sha1 "1a4f7e938b3f9afc35c948e3fbefa3b9467eaf43"
+  url "http://download.qt-project.org/official_releases/qtcreator/3.1/3.1.2/qt-creator-opensource-src-3.1.2.tar.gz"
+  sha1 "b231ce2311208bec1c61e50ed8e74bb6f8a4cf64"
 
-  head "https://git.gitorious.org/qt-creator/qt-creator.git", :branch => "3.1"
+  head "https://git.gitorious.org/qt-creator/qt-creator.git", :branch => "3.2"
 
   depends_on "qt5"
 
